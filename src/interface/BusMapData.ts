ea7d0fd8988605fdb6f8c7876e0111acb4cf0880
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-11-05 20:55:38 +0900

    busmapdataのinterface仮作成

---
+++ src/interface/BusMapData.ts
@@ -0,0 +1,57 @@
+import { UrRoute } from "./UrRoute";
+import { UrStop } from "./UrStop"
+import { Stop, StopTime, Trip } from "@come25136/gtfs";
+
+interface BusMapData {
+    /**
+     * GTFS-RTのバイナリ変換後そのままのデータ
+     */
+    rt: any;
+
+    /**
+     * Stop配列
+     */
+    stops: Array<Stop>;
+
+    /**
+     * 原子Stop配列
+     */
+    urStops: Array<UrStop>;
+
+    /**
+     * 親Stop配列
+     * @description 基本はUrStopのみ
+     * parent_stationsは平均をとって元データを無視し生成
+     */
+    parentStations: Array<Stop>;
+
+    /**
+     * 原子route配列
+     */
+    urRoutes: Array<UrRoute>;
+
+    // calendarの使いみちわかる方補足お願いします
+
+    /**
+     * ？？？？？？？？？？
+     */
+    calendar: Array<Object>;
+
+    /**
+     * 便配列
+     */
+    trips: Array<Trip>;
+
+    /**
+     * StopTimes配列
+     */
+    stopTimes: Array<StopTime>;
+}
+
+// l_bmd→c_bmdに変わる場面がある
+// データ加工後、何らかの「封印」状態に変化する？
+// クラスを作る際、その辺りも考慮した方が良さげ
+
+export {
+    BusMapData
+}
