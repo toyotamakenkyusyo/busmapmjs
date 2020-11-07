8fed2658e7021ae24d34b91b04e9665aa7a2d5f4
toyotamakenkyusyo <keiobe2016@yahoo.co.jp>
2020-11-07 14:40:41 +0900

    型コメント新方式化

--- mjs/f_sort_nodes.mjs
+++ mjs/f_sort_nodes.mjs
@@ -1,59 +1,75 @@
-//グラフを作り、整列した節点の列をつくる
-export function f_sort_nodes(a_walk_array/*: {
+/*
+interface i_walk_array {
 	"node_array": string[]; //節点idの列（グラフ1）
-}[]*/)/*: string[] //節点idの配列*/ {
-	
-	//グラフ1：全ての節点を含むグラフ
-	const c_graph_1 /*:{
-		"nodes" : { //節点達
-			[key: string]: { //節点id
-				"next_nodes": { // 隣接する節点達
-					[key: string]: string; //節点id
-				};
-				"degree": number; //次数
+}
+
+interface i_graph_1 {
+	"nodes" : { //節点達
+		[key: string]: { //節点id
+			"next_nodes": { // 隣接する節点達
+				[key: string]: string; //節点id
+			};
+			"degree": number; //次数
+		};
+	};
+}
+
+interface i_graph_2 {
+	"nodes": { //節点達
+		[key: string]: { //節点id
+			"next_nodes": { // 隣接する節点達
+				[key: string]: string; //節点id
+			};
+			"next_links": { // 隣接する辺達
+				[key: string]: string; //辺id
 			};
 		};
-	}*/= {"nodes":{}};
+	};
+	"links": { //辺達
+		[key: string]: { //辺id
+			"head": string; //始点の節点id
+			"tail": string; //始点の節点id
+			"node_array_1": string; //中間の節点idを_区切りでつないだ文字列
+			"node_array_2": string; //中間の節点idを_区切りでつないだ文字列
+			"code": number; //適当な順番をつけた正整数
+			"direction": number; //向き（1か-1）
+		};
+	};
+}
+
+interface i_walk_array_2 {
+	"link_array": { //辺の列（グラフ2）
+		"link_id": string; //辺id
+		"direction": number; //向き（1か-1）
+		"node_array_1": string; //節点idを順に_区切りでつないだ文字列
+		"node_array_2": string; //節点idを逆順に_区切りでつないだ文字列
+	}[];
+}
+
+*/
+
+
+
+//グラフを作り、整列した節点の列をつくる
+export function f_sort_nodes(
+	a_walk_array//: i_walk_array[]
+)//: string[] //節点idの配列
+{
+	
+	//グラフ1：全ての節点を含むグラフ
+	const c_graph_1 = {"nodes": {}};//as i_graph_1;
 	
 	//グラフ2：グラフ1から次数2の節点を除いたグラフ
 	//多重辺はないので問題ない。self-loopは？
-	const c_graph_2 /*: {
-		"nodes": { //節点達
-			[key: string]: { //節点id
-				"next_nodes": { // 隣接する節点達
-					[key: string]: string; //節点id
-				};
-				"next_links": { // 隣接する辺達
-					[key: string]: string; //辺id
-				};
-			};
-		};
-		"links": { //辺達
-			[key: string]: { //辺id
-				"head": string; //始点の節点id
-				"tail": string; //始点の節点id
-				"node_array_1": string; //中間の節点idを_区切りでつないだ文字列
-				"node_array_2": string; //中間の節点idを_区切りでつないだ文字列
-				"code": number; //適当な順番をつけた正整数
-				"direction": number; //向き（1か-1）
-			};
-		};
-	}*/ = {"nodes": {}, "links": {}};
+	const c_graph_2 = {"nodes": {}, "links": {}};//as i_graph_2;
 	
-	const c_walk_array_2/*: {
-		"link_array": { //辺の列（グラフ2）
-			"link_id": string; //辺id
-			"direction": number; //向き（1か-1）
-			"node_array_1": string; //節点idを順に_区切りでつないだ文字列
-			"node_array_2": string; //節点idを逆順に_区切りでつないだ文字列
-		}[];
-	}[]*/ = [];
+	const c_walk_array_2 = [];//as i_walk_array_2[];
 	
 	
 	//入力データからグラフ1をつくる
 	for (let i1 = 0; i1 < a_walk_array.length; i1++) {
 		for (let i2 = 0; i2 < a_walk_array[i1]["node_array"].length; i2++) {
-			const c_node_id/*: string*/ = a_walk_array[i1]["node_array"][i2]; //節点id
+			const c_node_id = a_walk_array[i1]["node_array"][i2];//as string; //節点id
 			if (c_graph_1["nodes"][c_node_id] === undefined) { //まだその節点がないなら
 				//節点を追加
 				c_graph_1["nodes"][c_node_id] = {
@@ -62,12 +78,12 @@ export function f_sort_nodes(a_walk_array/*: {
 				};
 			}
 			if (i2 !== 0) { //最初以外
-				const c_from_node_id/*: string*/ = a_walk_array[i1]["node_array"][i2 - 1]; //前の節点の節点id
+				const c_from_node_id = a_walk_array[i1]["node_array"][i2 - 1];//as string; //前の節点の節点id
 				//前の節点を隣接する節点達に追加（重複あり）
 				c_graph_1["nodes"][c_node_id]["next_nodes"][c_from_node_id] = c_from_node_id;
 			}
 			if (i2 !== a_walk_array[i1]["node_array"].length - 1) { //最後以外
-				const c_to_node_id/*: string*/ = a_walk_array[i1]["node_array"][i2 + 1]; //後の節点の節点id
+				const c_to_node_id = a_walk_array[i1]["node_array"][i2 + 1];//as string; //後の節点の節点id
 				//後の節点を隣接する節点達に追加（重複あり）
 				c_graph_1["nodes"][c_node_id]["next_nodes"][c_to_node_id] = c_to_node_id; 
 			}
@@ -87,12 +103,12 @@ export function f_sort_nodes(a_walk_array/*: {
 	
 	
 	for (let i1 = 0; i1 < a_walk_array.length; i1++) {
-		let l_head/*: string*/ = null; //最初の節点id
-		let l_node_array_1/*: string*/ = "_"; //節点idを_区切りで順に繋げた文字列
-		let l_node_array_2/*: string*/ = "_"; //節点idを_区切りで逆順に繋げた文字列
+		let l_head = null;//as string; //最初の節点id
+		let l_node_array_1 = "_";//as string; //節点idを_区切りで順に繋げた文字列
+		let l_node_array_2 = "_";//as string; //節点idを_区切りで逆順に繋げた文字列
 		//次数2でない節点（端の点、分岐点）で分割してリンクをつくる
 		for (let i2 = 0; i2 < a_walk_array[i1]["node_array"].length; i2++) {
-			const c_node_id /*: string*/ = a_walk_array[i1]["node_array"][i2]; //節点id
+			const c_node_id = a_walk_array[i1]["node_array"][i2];//as string; //節点id
 			l_node_array_1 = l_node_array_1 + c_node_id + "_";
 			l_node_array_2 = "_" + c_node_id + l_node_array_2;
 			if (c_graph_1["nodes"][c_node_id]["degree"] === 2) { //次数2、途中の点
@@ -163,22 +179,23 @@ export function f_sort_nodes(a_walk_array/*: {
 		}
 	}
 	
+	/*
+	interface i_first_last {
+		"link_id": string; //辺id、未定はnull
+		"direction": number; //向き1か-1、未定はnull
+		"node_array_1": string; //節点idを順に_区切りでつないだ文字列
+		"node_array_2": string; //節点idを逆順に_区切りでつないだ文字列
+	}
+	
+	*/
+	
+	
 	//端の一部のやつ（端の節点の次数が2でない）の節点idと向きを求める
 	//含まれる辺idを探す
 	//同じ処理が必要なやつが途中にあるとうまくいかない？
 	for (let i1 = 0; i1 < c_walk_array_2.length; i1++) {
-		const c_first/*: {
-			"link_id": string; //辺id、未定はnull
-			"direction": number; //向き1か-1、未定はnull
-			"node_array_1": string; //節点idを順に_区切りでつないだ文字列
-			"node_array_2": string; //節点idを逆順に_区切りでつないだ文字列
-		}*/ = c_walk_array_2[i1]["link_array"][0];
-		const c_last /*: {
-			"link_id": string; //辺id、未定はnull
-			"direction": number; //向き1か-1、未定はnull
-			"node_array_1": string; //節点idを順に_区切りでつないだ文字列
-			"node_array_2": string; //節点idを逆順に_区切りでつないだ文字列
-		}*/= c_walk_array_2[i1]["link_array"][c_walk_array_2[i1]["link_array"].length - 1];
+		const c_first = c_walk_array_2[i1]["link_array"][0];//as i_first_last;
+		const c_last = c_walk_array_2[i1]["link_array"][c_walk_array_2[i1]["link_array"].length - 1];//as i_first_last;
 		if (c_first["link_id"] === null) {
 			for (let i2 in c_graph_2["links"]) {
 				if (i2.indexOf(c_first["node_array_1"]) !== -1) { //辺idの文字列が含まれる
@@ -210,29 +227,37 @@ export function f_sort_nodes(a_walk_array/*: {
 	console.log(c_graph_2);
 	
 	//グラフ2の辺に適当な順番をつける（順番は向き付けに使う）
-	let l_count /*: number*/ = 1; //連続した正整数
+	let l_count = 1;// as number; //連続した正整数
 	for (let i1 in c_graph_2["links"]) {
 		c_graph_2["links"][i1]["code"] = l_count;
 		l_count += 1;
 	}
 	
 	
-	const c_acyclic/*: {
+	/*
+	interface i_acyclic {
 		"code": number; //向きをつける番号
 		"score": number; //点数
-	}*/ = [];
+	}
+	
+	interface i_next_nodes {
+		[key: string]: { //節点id
+			"next_nodes": {
+				[key: string]: string; //節点id
+			};
+			"cyclic": boolean; //巡回したらtrue
+		};
+	}
+	
+	*/
+	
+	
+	const c_acyclic = [];//as i_acyclic;
 	//2 ** l_count通りについて、ループの有無を判定し、点数をつける
 	for (let i1 = 1; i1 < 2 ** l_count; i1++) {
-		const c_next_nodes/*: {
-			[key: string]: { //節点id
-				"next_nodes": {
-					[key: string]: string; //節点id
-				};
-				"cyclic": boolean; //巡回したらtrue
-			};
-		}*/ = {}; //ここに隣接する節点の節点idを入れていく。
+		const c_next_nodes = {};//as i_next_nodes; //ここに隣接する節点の節点idを入れていく。
 		for (let i2 in c_graph_2["links"]) {
-			const c_direction/*: number*/ = -1 + 2 * (Math.floor(i1 / (2 ** c_graph_2["links"][i2]["code"])) % 2); //向き（-1か1）
+			const c_direction = -1 + 2 * (Math.floor(i1 / (2 ** c_graph_2["links"][i2]["code"])) % 2);//as number; //向き（-1か1）
 			c_graph_2["links"][i2]["direction"] = c_direction;
 			if (c_direction === -1) {
 				if (c_next_nodes[c_graph_2["links"][i2]["head"]] === undefined) {
@@ -258,7 +283,7 @@ export function f_sort_nodes(a_walk_array/*: {
 			let l_exist/*: boolean*/ = false;
 			for (let i2 in c_next_nodes) {
 				if (c_next_nodes[i2]["cyclic"] !== false) {
-					let l_next_node_count/*: number*/ = Object.keys(c_next_nodes[i2]["next_nodes"]).length;
+					let l_next_node_count = Object.keys(c_next_nodes[i2]["next_nodes"]).length;//as number;
 					for (let i3 in c_next_nodes[i2]["next_nodes"]) {
 						if (c_next_nodes[i3] === undefined) {
 							l_next_node_count -= 1;
@@ -276,7 +301,7 @@ export function f_sort_nodes(a_walk_array/*: {
 				f_check_cyclic();
 			}
 		}
-		let l_exist/*: boolean*/ = false; //巡回があればtrue
+		let l_exist = false;//as boolean; //巡回があればtrue
 		for (let i2 in c_next_nodes) {
 			if (c_next_nodes[i2]["cyclic"] !== false) {
 				l_exist = true; //巡回あり
@@ -284,11 +309,11 @@ export function f_sort_nodes(a_walk_array/*: {
 		}
 		if (l_exist === false) { //巡回なし
 			//点数をつける
-			let l_score/*: number*/ = 0; //点数（大きいほどよい）
+			let l_score = 0;//as number; //点数（大きいほどよい）
 			for (let i2 = 0; i2 < c_walk_array_2.length; i2++) {
 				for (let i3 = 0; i3 < c_walk_array_2[i2]["link_array"].length - 1; i3++) {
-					const c_section_1/*: string*/ = c_walk_array_2[i2]["link_array"][i3]["link_id"];
-					const c_section_2/*: string*/ = c_walk_array_2[i2]["link_array"][i3 + 1]["link_id"];
+					const c_section_1 = c_walk_array_2[i2]["link_array"][i3]["link_id"];//as string;
+					const c_section_2 = c_walk_array_2[i2]["link_array"][i3 + 1]["link_id"];//as string;
 					l_score += c_graph_2["links"][c_section_1]["direction"] * c_walk_array_2[i2]["link_array"][i3]["direction"] * c_graph_2["links"][c_section_2]["direction"] * c_walk_array_2[i2]["link_array"][i3 + 1]["direction"];
 				}
 			}
@@ -305,8 +330,8 @@ export function f_sort_nodes(a_walk_array/*: {
 	
 	
 	//最大になる向き付け
-	let l_max_score/*: number*/ = Number.MIN_SAFE_INTEGER; //最高点
-	let l_argmax_score/*: number*/ = null; //最高点の向き付け
+	let l_max_score = Number.MIN_SAFE_INTEGER;//as number; //最高点
+	let l_argmax_score = null;//as number; //最高点の向き付け
 	for (let i1 = 0; i1 < c_acyclic.length; i1++) {
 		if (l_max_score <= c_acyclic[i1]["score"]) {
 			l_max_score = c_acyclic[i1]["score"];
@@ -320,17 +345,25 @@ export function f_sort_nodes(a_walk_array/*: {
 	}
 	
 	
-	const c_link_array/*{
+	/*
+	interface i_link_array {
 		"id": string; //辺id
 		"direction": number; //向き（1か-1）
-	}[]*/ = []; //最終的な並べ方
+	}
+	
+	*/
+	
+	
+	const c_link_array = [];//as i_link_array[]; //最終的な並べ方
 	//トポロジカルソート
 	for (let i1 in c_graph_2["links"]) {
 		f_add(i1);
-		function f_add(a/*: string*/) {
+		function f_add(
+			a//: string
+		) {
 			if (c_graph_2["links"][a]["add"] !== true) {
 				c_graph_2["links"][a]["add"] = true;
-				let l_next_node/*: string*/ = null;
+				let l_next_node = null; //as string;
 				if (c_graph_2["links"][a]["direction"] === 1) {
 					l_next_node = c_graph_2["links"][a]["tail"];
 				} else if (c_graph_2["links"][a]["direction"] === -1) {
@@ -356,21 +389,27 @@ export function f_sort_nodes(a_walk_array/*: {
 	console.log(c_link_array);
 	
 	
-	const c_node_array/*: {
+	/*
+	interface i_node_array {
 		"node_id": string; //節点id
 		"head": boolean; //始点はtrue
 		"tail": boolean; //終点はtrue
 		"delete": boolean; //消すものはtrue
-	}[]*/ = [];
+	}
+	
+	*/
+	
+	
+	const c_node_array = [];//as i_node_array[];
 	
 	for (let i1 = 0; i1 < c_link_array.length; i1++) {
-		const c_ids/*: string*/ = c_link_array[i1]["id"].split("_");
-		const c_direction/*: number*/ = c_link_array[i1]["direction"];
+		const c_ids = c_link_array[i1]["id"].split("_");//as string;
+		const c_direction = c_link_array[i1]["direction"];//as number;
 		if (c_direction === 1) {
 			for (let i2 = 1; i2 < c_ids.length - 1; i2++) {
-				let l_head/*: boolean*/ = false; //始点はtrue
-				let l_tail/*: boolean*/ = false; //終点はtrue
-				let l_delete/*: boolean*/ = false;
+				let l_head = false;//as boolean; //始点はtrue
+				let l_tail = false;//as boolean; //終点はtrue
+				let l_delete = false;//as boolean;
 				if (i2 === 1) {
 					l_head = true;
 					l_delete = true; //始点と終点は仮に全部trueにする
@@ -388,9 +427,9 @@ export function f_sort_nodes(a_walk_array/*: {
 			}
 		} else if (c_direction === -1) {
 			for (let i2 = c_ids.length - 2; i2 >= 1; i2--) {
-				let l_head/*: boolean*/ = false; //始点はtrue
-				let l_tail/*: boolean*/ = false; //終点はtrue
-				let l_delete/*: boolean*/ = false;
+				let l_head = false;//as boolean; //始点はtrue
+				let l_tail = false;//as boolean; //終点はtrue
+				let l_delete = false;//as boolean;
 				if (i2 === c_ids.length - 2) {
 					l_head = true;
 					l_delete = true; //始点と終点は仮に全部trueにする
@@ -411,7 +450,7 @@ export function f_sort_nodes(a_walk_array/*: {
 	
 	//複数ある節点を1つにしぼる
 	for (let i1 in c_graph_2["nodes"]) {
-		let l_exist/*: boolean*/ = false; //あったらtrue
+		let l_exist = false;//as boolean; //あったらtrue
 		for (let i2 = 0; i2 < c_node_array.length; i2++) {
 			if (c_node_array[i2]["node_id"] === i1 && c_node_array[i2]["head"] === true) {
 				c_node_array[i2]["delete"] = false; //最初に始点にでたときに残す
@@ -430,7 +469,7 @@ export function f_sort_nodes(a_walk_array/*: {
 		}
 	}
 	
-	const c_node_array_2/*: string[]*/ = []; //節点idのみの列
+	const c_node_array_2 = [];//as string[]; //節点idのみの列
 	for (let i1 = 0; i1 < c_node_array.length; i1++) {
 		if (c_node_array[i1]["delete"] === false) {
 			c_node_array_2.push(c_node_array[i1]["node_id"]);
